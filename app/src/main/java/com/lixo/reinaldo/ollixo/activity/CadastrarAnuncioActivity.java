ca06863e78152689566b179210b16dace254dc51
reinaldo22 <vargas.reynaldo22@gmail.com>
2019-02-04 22:13:50 -0400

    Permissoes e mascara de telefone e moeda

--- app/src/main/java/com/lixo/reinaldo/ollixo/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/lixo/reinaldo/ollixo/activity/CadastrarAnuncioActivity.java
@@ -1,22 +1,93 @@
 package com.lixo.reinaldo.ollixo.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
-import android.view.LayoutInflater;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.lixo.reinaldo.ollixo.R;
+import com.lixo.reinaldo.ollixo.helper.Permissoes;
+import com.santalu.maskedittext.MaskEditText;
+
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
+
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
 
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
+        inicializarComponentes();
 
     }
+
+    public void salvarAnuncio(View view){
+
+        String valor = campoTelefone.getText().toString();
+        Log.d("salvar", "salvarAnuncio: " + valor );
+
+    }
+
+    private void inicializarComponentes(){
+
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+
+        //Configura localidade para pt -> portugues BR -> Brasil
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale( locale );
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for( int permissaoResultado : grantResults ){
+            if( permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+
+    }
+
+    private void alertaValidacaoPermissao(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
 }

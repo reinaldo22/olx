2d3b9eadcbc256e4386300556990bdefce579d63
reinaldo22 <vargas.reynaldo22@gmail.com>
2019-02-05 13:17:57 -0400

    Escolha de imagens para o app

--- app/src/main/java/com/lixo/reinaldo/ollixo/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/lixo/reinaldo/ollixo/activity/CadastrarAnuncioActivity.java
@@ -1,32 +1,43 @@
 package com.lixo.reinaldo.ollixo.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.lixo.reinaldo.ollixo.R;
 import com.lixo.reinaldo.ollixo.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity
+                implements View.OnClickListener{
 
     private EditText campoTitulo, campoDescricao;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
+    private ImageView imagem1,imagem2,imagem3;
 
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,7 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    //Metodo de salvar um novo anuncio
     public void salvarAnuncio(View view){
 
         String valor = campoTelefone.getText().toString();
@@ -47,19 +59,73 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+    }
+
+    private void escolherImagem( int requestCode) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if( resultCode == Activity.RESULT_OK){
+
+            //Recupera a imagem
+            Uri imageSelecionada = data.getData();
+            String caminhoImagem = imageSelecionada.toString();
+
+            //Configurar imagem de imageView
+            if( requestCode == 1){
+                imagem1.setImageURI( imageSelecionada );
+            }else if( requestCode == 2){
+                imagem2.setImageURI( imageSelecionada );
+            }else if( requestCode == 3){
+                imagem3.setImageURI( imageSelecionada );
+            }
+            listaFotosRecuperadas.add(caminhoImagem);
+        }
+
+    }
+
+    //Metodo de inicializar componentes
     private void inicializarComponentes(){
 
         campoTitulo = findViewById(R.id.editTitulo);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editValor);
         campoTelefone = findViewById(R.id.editTelefone);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
+
 
         //Configura localidade para pt -> portugues BR -> Brasil
         Locale locale = new Locale("pt", "BR");
         campoValor.setLocale( locale );
 
     }
-
+    //Metodo de permitir publicacao de imagens
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -72,6 +138,7 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
+    //Metodo de alerta de permissao de publicacao de anuncio
     private void alertaValidacaoPermissao(){
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);

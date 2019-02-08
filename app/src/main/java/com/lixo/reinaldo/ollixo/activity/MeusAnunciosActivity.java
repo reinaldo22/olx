ac5c4f43927b94ecbbc1dbb20926d95556f25762
reinaldo22 <vargas.reynaldo22@gmail.com>
2019-02-07 20:49:08 -0400

    Listagem dos anuncios

--- app/src/main/java/com/lixo/reinaldo/ollixo/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/lixo/reinaldo/ollixo/activity/MeusAnunciosActivity.java
@@ -2,23 +2,48 @@ package com.lixo.reinaldo.ollixo.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.lixo.reinaldo.ollixo.R;
+import com.lixo.reinaldo.ollixo.adapter.AdapterAnuncios;
+import com.lixo.reinaldo.ollixo.helper.ConfiguracaoFirebase;
+import com.lixo.reinaldo.ollixo.model.Anuncio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncios adapterAnuncios;
+    private DatabaseReference anuncioUsuarioRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-
         setContentView(R.layout.activity_meus_anuncios);
+
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child(ConfiguracaoFirebase.getIdUsuario());
+
+        inicializarComponentes();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,6 +55,40 @@ public class MeusAnunciosActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Configurar RecyclerView
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncios = new AdapterAnuncios(anuncios, this);
+        recyclerAnuncios.setAdapter(adapterAnuncios);
+
+        //metodo que pega os anuncios
+        recuperarAnuncios();
+    }
+
+    private void recuperarAnuncios() {
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                anuncios.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    anuncios.add(ds.getValue( Anuncio.class ) );
+                }
+                Collections.reverse(anuncios);
+                adapterAnuncios.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void inicializarComponentes() {
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 
 }
---
+++ app/src/main/java/com/lixo/reinaldo/ollixo/adapter/AdapterAnuncios.java
@@ -0,0 +1,76 @@
+package com.lixo.reinaldo.ollixo.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lixo.reinaldo.ollixo.R;
+import com.lixo.reinaldo.ollixo.model.Anuncio;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterAnuncios extends RecyclerView.Adapter<AdapterAnuncios.MyViewHolder>{
+
+    private List<Anuncio> anuncios;
+    private Context context;
+
+    public AdapterAnuncios(List<Anuncio> anuncios, Context context) {
+        this.anuncios = anuncios;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View item = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_anuncio,viewGroup,false);
+
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int posicao) {
+
+        Anuncio anuncio = anuncios.get(posicao);
+        myViewHolder.titulo.setText(anuncio.getTitulo());
+        myViewHolder.valor.setText(anuncio.getValor());
+
+        //Pega a primeira imagem da lista
+        List<String> urlFotos = anuncio.getFotos();
+        String urlCapa = urlFotos.get(0);
+
+        Picasso.get().load(urlCapa).into(myViewHolder.foto);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return anuncios.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titulo;
+        TextView valor;
+        ImageView foto;
+
+        public MyViewHolder(View itemView){
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            valor = itemView.findViewById(R.id.textPreco);
+            foto = itemView.findViewById(R.id.imageAnuncio);
+
+
+
+        }
+    }
+
+}
